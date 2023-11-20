fcbf2324caeba8857e23b9f81eccaedca4052e7e
retro <retrowew@gmail.com>
2023-11-20 22:32:12 +0100

    hw 1

---
+++ 1/1.1.js
@@ -0,0 +1,24 @@
+s1 = 'hello'
+console.log(s1)
+s2 = 'owu'
+console.log(s2)
+s3 = 'com'
+console.log(s3)
+s4 = 'ua'
+console.log(s4)
+n1 = 1
+console.log(n1)
+n2 = 10
+console.log(n2)
+n3 = -999
+console.log(n3)
+n4 = 3.14
+console.log(n4)
+n5 = 2.7
+console.log(n5)
+n6 = 16
+console.log(n6)
+b1 = true
+console.log(b1)
+b2 = false
+console.log(b2)

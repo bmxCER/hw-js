2707a276e062653bbe795df59122682a649d150a
retro <retrowew@gmail.com>
2023-11-23 13:54:18 +0100

    hw 2

--- 2/2.1.js
+++ 2/2.1.js
@@ -10,5 +10,28 @@ let user1 = {
     ifHasDog: true,
     ifHasWife: false
 }
+
 console.log(user1)
-console.log(user1.password)
+
+ let users = [
+     {user1: 'Andrey', username: 'aswf', passsword:'asdg'},
+     {user2: 'Danil', username: 'sdfh', passsword:'gjhk'},
+     {user3: 'Vasilii', username: 'SGSDFH', passsword:'gfjd'},
+     {user4: 'Denis', username: 'hgk', passsword:'dghjk'},
+     {user5: 'Nastya', username: 'dfgjh', passsword:'sgfjh'},
+     {user6: 'Dasha', username: 'dfhdf', passsword:'sfgj'},
+     {user7: 'Maxim', username: 'dfhadfa', passsword:'dfahafd'},
+     {user8: 'Katya', username: 'saghhadf', passsword:'arhya'},
+     {user9: 'Dima', username: 'adfh', passsword:'adfh'},
+     {user10: 'Zhenya', username: 'fdbs', passsword:'rytj'},
+]
+console.log(users[0].passsword)
+console.log(users[1].passsword)
+console.log(users[2].passsword)
+console.log(users[3].passsword)
+console.log(users[4].passsword)
+console.log(users[5].passsword)
+console.log(users[6].passsword)
+console.log(users[7].passsword)
+console.log(users[8].passsword)
+console.log(users[9].passsword)
